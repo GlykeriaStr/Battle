d1cef817c6673b3a2c2efbc9516d972ed338f682
Morne van Zyl <Morne@Mornes-MacBook-Air.local>
2021-01-20 16:31:41 +0000

    Updated app with POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -1,16 +1,25 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  # app code here...
+  
+  enable :sessions
 
   get '/' do
     erb(:index)
-    end
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1_name]
+    @player_2 = session[:player_2_name]
+    erb(:play)
+  end
 
   post '/names' do 
-    @player_1 = params[:player_1_name]
-    @player_2 = params[:player_2_name]
-    erb(:play)
+    # @player_1 = params[:player_1_name]
+    # @player_2 = params[:player_2_name]
+    session['player_1_name'] = params[:player_1_name]
+    session['player_2_name'] = params[:player_2_name]
+    redirect to('/play')
   end
   
   run! if app_file == $0
