d5dfc4812eae5c74e01f4ccac53439347812fa14
Glykeria Stravodimou <strglykeria@gmail.com>
2021-01-21 14:58:48 +0000

    Player 2 HP

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
-feature 'entering names' do 
-    scenario 'allow two players to enter their names' do 
-        visit('/')
-        fill_in('player_1_name', with: 'Morne')
-        fill_in('player_2_name', with: 'Meg')
-        click_button 'Submit'
-        expect(page).to have_content 'Morne vs. Meg'
-    end 
-end 
+feature 'entering names' do
+    scenario 'allow two players to enter their names' do
+        sign_in_and_play
+        expect(page).to have_content 'Boris vs. Nicola'
+    end
+end
--- spec/features/viewing_hit_points_spec.rb
+++ spec/features/viewing_hit_points_spec.rb
@@ -1,9 +1,6 @@
-feature 'viewing hit points' do 
-    scenario 'display hit points for players' do 
-        visit('/')
-        fill_in('player_1_name', with: 'Boris')
-        fill_in('player_2_name', with: 'Nicola')
-        click_button 'Submit'
-        expect(page).to have_content '60 HP'
-    end 
-end 
+feature 'viewing hit points' do
+    scenario 'display hit points for players' do
+        sign_in_and_play
+        expect(page).to have_content 'Nicola: 60HP'
+    end
+end
